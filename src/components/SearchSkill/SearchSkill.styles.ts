942c316172db06d7e90fb16bd7199362831a7bf6
Nijin Nazar <nijin@qburst.com>
2023-10-26 17:34:12 +0530

    Add: Search Skill Component

--- src/components/SearchSkill/SearchSkill.styles.ts
+++ src/components/SearchSkill/SearchSkill.styles.ts
@@ -1,1 +1,82 @@
 import styled from "styled-components";
+
+export const SkillSearchContainer = styled.div`
+  position: relative;
+  width: 30%;
+  height: 100%;
+
+  & > div {
+    display: flex;
+    border: 1px solid #ccc;
+    padding: 2px;
+    border-radius: 8px;
+  }
+`;
+
+export const SelectedSkills = styled.div`
+  display: flex;
+  height: 100%;
+  max-width: 260px;
+  font-size: 12px;
+  gap: 4px;
+  color: var(--font-color);
+  overflow-x: auto;
+  scrollbar-width: thin;
+`;
+
+export const SelectedSkillTag = styled.p`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 6px;
+  border-radius: 50px;
+  padding: 8px;
+
+  &:nth-child(even) {
+    background-color: ${(props) => props.theme.primary};
+    color: ${(props) => props.theme.secondary};
+  }
+
+  &:nth-child(odd) {
+    background-color: ${(props) => props.theme.secondary};
+    color: ${(props) => props.theme.primary};
+  }
+
+  & > .remove-skill-btn {
+    cursor: pointer;
+  }
+`;
+
+export const SearchSkillInput = styled.input`
+  padding: 0 6px;
+  font-size: 18px;
+  flex: 1;
+  border: none;
+  outline: none;
+`;
+
+export const DropDownContent = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  max-height: 180px;
+  overflow: auto;
+  border-radius: none;
+
+  & > ul {
+    list-style: none;
+    padding: 0;
+    width: 100%;
+  }
+
+  & > ul > li {
+    padding: 10px;
+    cursor: pointer;
+    transition: 0.3s;
+  }
+
+  & > ul > li:hover {
+    background-color: ${(props) => props.theme.primary};
+    color: ${(props) => props.theme.secondary};
+  }
+`;
--- src/components/SearchSkill/SearchSkill.tsx
+++ src/components/SearchSkill/SearchSkill.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  DropDownContent,
+  SearchSkillInput,
+  SelectedSkills,
+  SelectedSkillTag,
+  SkillSearchContainer,
+} from "./SearchSkill.styles";
+
+import { IoIosRemoveCircleOutline } from "react-icons/io";
 
 const SearchSkill = () => {
-  return <div>SearchSkill</div>;
+  const [showList, setShowList] = useState(false);
+
+  return (
+    <SkillSearchContainer>
+      <div>
+        <SelectedSkills>
+          <SelectedSkillTag>
+            <span>HTML</span>
+            <IoIosRemoveCircleOutline className="remove-skill-btn" />
+          </SelectedSkillTag>
+        </SelectedSkills>
+        <SearchSkillInput
+          onFocus={() => setShowList(true)}
+          onBlur={() => setShowList(false)}
+        />
+      </div>
+      {showList && (
+        <DropDownContent>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>ReactJs</li>
+            <li>Angular</li>
+          </ul>
+        </DropDownContent>
+      )}
+    </SkillSearchContainer>
+  );
 };
 
 export default SearchSkill;
